27e6cee9222c9dcedaae653f22e37889295dbb49
shuaiL8 <fred-liu@outlook.com>
2019-02-28 02:54:47 -0500

    navagation bar

---
+++ app/src/main/java/com/example/fitawear/HomeFragment.kt
@@ -0,0 +1,46 @@
+package com.example.fitawear
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.*
+import android.view.MenuInflater
+
+
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view: View = inflater!!.inflate(R.layout.fragment_home, container,
+            false)
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.tool_bar, menu)
+        val add = menu.findItem(R.id.add)
+        add.isVisible = false
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.share -> {
+            // User chose the "Settings" item, show the app settings UI...
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+}
--- app/src/main/java/com/example/fitawear/MainActivity.kt
+++ app/src/main/java/com/example/fitawear/MainActivity.kt
@@ -4,35 +4,108 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+//                val actionBar = supportActionBar
+//                actionBar?.show()
+
+                val toolbarTiltle = findViewById<TextView>(R.id.toolbar_title)
+                toolbarTiltle.text = "FitAwear"
+
+                val homeFragment = HomeFragment()
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.content, homeFragment).addToBackStack(null).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+//                val actionBar = supportActionBar
+//                actionBar?.show()
+
+                val toolbarTiltle = findViewById<TextView>(R.id.toolbar_title)
+                toolbarTiltle.text = "Dashboard"
+
+                val dashboardFragment = DashboardFragment()
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.content, dashboardFragment).addToBackStack(null).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+//                val actionBar = supportActionBar
+//                actionBar?.show()
+
+                val toolbarTiltle = findViewById<TextView>(R.id.toolbar_title)
+                toolbarTiltle.text = "Awards"
+
+                val awardsFragment = AwardsFragment()
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.content, awardsFragment).addToBackStack(null).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_me -> {
-                message.setText(R.string.title_me)
+//                val actionBar = supportActionBar
+//                actionBar?.hide()
+
+                val toolbarTiltle = findViewById<TextView>(R.id.toolbar_title)
+                toolbarTiltle.text = ""
+
+                val meFragment = MeFragment()
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.content, meFragment).addToBackStack(null).commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu to use in the action bar
+//        val inflater = menuInflater
+//        inflater.inflate(R.menu.tool_bar, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+//        R.id.share -> {
+//            // User chose the "Settings" item, show the app settings UI...
+//            true
+//        }
+//
+//        else -> {
+//            // If we got here, the user's action was not recognized.
+//            // Invoke the superclass to handle it.
+//            super.onOptionsItemSelected(item)
+//        }
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val actionBar = supportActionBar
+        actionBar!!.title = ""
+
+
+        val homeFragment = HomeFragment()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.add(R.id.content, homeFragment).addToBackStack(null).commit()
     }
+
+
 }
